8d7e482b33d900c40b83086e6e4153196d323774
Sonya <iamsonche@gmail.com>
2026-05-11 21:08:45 +0300

    external_sort.py complete
    Implement external sorting using merge sort with heapq.

--- Programming/sem2/lab5/external_sort.py
+++ Programming/sem2/lab5/external_sort.py
@@ -1,6 +1,9 @@
 import csv
 import time # для отслеживания времени
 
+import heapq # куча для сортировки
+import os # для работы с файлами
+
 def choose_key(key_name):
     hat = {
         "дата": 0,
@@ -50,3 +53,45 @@ def split_file(input_file, key_index):
 
         print("Split time:", time.time() - start_time)
         return temp_files, header
+
+# функция сортировки
+def merge_files(temp_files, output_file, key_index, header):
+    start_time = time.time()
+
+    files = [open(f, newline='', encoding='utf-8-sig') for f in temp_files]
+    readers = [csv.reader(f) for f in files]
+
+    heap = []
+
+    for i, reader in enumerate(readers):
+        row = next(reader, None)
+        if row:
+            heapq.heappush(heap, (row[key_index], i, row)) # кладем в кучу кортеж
+
+    with open(output_file, 'w', newline='', encoding='utf-8-sig') as res:
+        writer = csv.writer(res)
+        writer.writerow(header) # записываем заголовок
+
+        while heap:
+            _, i, row = heapq.heappop(heap) # ключ, файл (i), строка (row)
+            writer.writerow(row)
+
+            next_row = next(readers[i], None)
+            if next_row:
+                heapq.heappush(heap, (next_row[key_index], i, next_row)) # если строка есть - кладем в кучу
+
+    for f in files:
+        f.close()
+
+    for f in temp_files:
+        os.remove(f)
+
+    print("Merge time:", time.time() - start_time)
+
+def external_sort(input_file, output_file, key_name):
+    key_index = choose_key(key_name)
+
+    temp_files, header = split_file(input_file, key_index)
+    merge_files(temp_files, output_file, key_index, header)
+
+external_sort("data.csv", "sorted.csv", "дата")
